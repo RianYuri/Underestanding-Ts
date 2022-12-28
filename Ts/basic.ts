d322050f277fb90b6ea0fb896d09c823a596a4d8
Rian Yuri <rianyurirgt@gmail.com>
2022-12-28 15:03:30 -0300

    Ts: Literal types

--- Ts/basic.ts
+++ Ts/basic.ts
@@ -31,4 +31,22 @@ function add( n1:number, n2:number , showResult: boolean,phrase:string ){
         console.log(hobby.toUpperCase());
         // console.log(hobby.map)
     }
-    
+
+    /*----------------------------------- Union Types -----------------------------------------------------*/
+function combine (
+    input1: number | string,
+    input2: number| string){
+    let result;
+    if(typeof input1 === "number" && typeof input2 === "number"){
+        
+        result = input1 + input2;
+    }else{
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinedAges = combine(30, 29);
+console.log(combinedAges);
+
+const combinedNames = combine('Rian', 'Yuri');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,16 +1,24 @@
-/*----------------------------------- Union Types -----------------------------------------------------*/
-function combine (input1: number | string, input2: number| string){
-    let result;
-    if(typeof input1 === "number" && typeof input2 === "number"){
-        
-        result = input1 + input2;
-    }else{
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-}
-const combinedAges = combine(30, 29);
-console.log(combinedAges);
-
-const combinedNames = combine('Rian', 'Yuri');
-console.log(combinedNames);
+    /*----------------------------------- Literal Types -----------------------------------------------------*/
+    function combine(
+        input1: number | string,
+        input2: number | string,
+        resultConversion: 'as-number' | 'as-text'
+      ) {
+        let result;
+        if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+          result = +input1 + +input2;
+        } else {
+          result = input1.toString() + input2.toString();
+        }
+        return result;
+      }
+      
+      const combinedAges = combine(30, 26, 'as-number');
+      console.log(combinedAges);
+      
+      const combinedStringAges = combine('30', '26', 'as-number');
+      console.log(combinedStringAges);
+      
+      const combinedNames = combine('Max', 'Anna', 'as-text');
+      console.log(combinedNames);
+      
